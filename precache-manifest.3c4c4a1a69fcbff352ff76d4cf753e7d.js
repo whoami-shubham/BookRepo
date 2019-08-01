581a127f95ab376cba017e809a4aad40bd728254
whoami-shubham <shubhamkumarjha0013@gmail.com>
2019-08-01 17:03:40 +0530

    Updates

---
+++ precache-manifest.3c4c4a1a69fcbff352ff76d4cf753e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f547be7fc2f4a58e621e7319eaee56",
+    "url": "/BookRepo/index.html"
+  },
+  {
+    "revision": "98cc5956f4d8fb8defcc",
+    "url": "/BookRepo/static/css/main.1f3f011e.chunk.css"
+  },
+  {
+    "revision": "d68d985194847b5f5fa8",
+    "url": "/BookRepo/static/js/2.e290fc1e.chunk.js"
+  },
+  {
+    "revision": "98cc5956f4d8fb8defcc",
+    "url": "/BookRepo/static/js/main.6d9a24bc.chunk.js"
+  },
+  {
+    "revision": "563473c0440558fbe32c",
+    "url": "/BookRepo/static/js/runtime~main.a91a4d2c.js"
+  }
+]);
